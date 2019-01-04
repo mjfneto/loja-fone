342173f2aca45168fdd96b76e08d507a0c3c1701
Mario Freire <huege.design@gmail.com>
2019-01-04 12:02:31 -0200

    Notas e mods 014.008 "classList.toggle, magia boa demais"

--- js/description_product.js
+++ js/description_product.js
@@ -13,10 +13,5 @@ const $heart = window.document.querySelector(".-heart");
 $heart.addEventListener("click", handleClick);
 
 function handleClick() {
-    if ($heart.classList.contains('-active')) {
-        $heart.classList.remove('-active');
-    }
-    else {
-        $heart.classList.add('-active');
-    }
+    $heart.classList.toggle('-active');
 };
