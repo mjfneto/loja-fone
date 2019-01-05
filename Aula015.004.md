0e5bd0234efe2fc73c7fe6075657befa809a865e
Mario Freire <huege.design@gmail.com>
2019-01-05 17:44:35 -0200

    Notas e mods 015.004 "Como funciona uma lista"

--- Aula015.004.md
+++ Aula015.004.md
@@ -4,3 +4,7 @@
 
 ## 002 "Não tem valor só saber JavaScript"
 
+## 003 "Mudando apenas uma estrela"
+
+## 004 "Como funciona uma lista"
+
--- js/description_product.js
+++ js/description_product.js
@@ -9,16 +9,39 @@ Com o clique do usuário, mostrar um aviso na tela. OK
 */
 
 const $heart = document.querySelector(".-heart");
-const $firstStar = document.querySelector(".star");
 
-$heart.addEventListener("click", handleClick);
-$firstStar.addEventListener("click", handleClickFirstStar);
+const $stars = document.querySelectorAll(".star");
 
-function handleClick() {
+$heart.addEventListener("click", handleClickHeart);
+
+$stars[0].addEventListener("click", handleClickStar0);
+$stars[1].addEventListener("click", handleClickStar1);
+$stars[2].addEventListener("click", handleClickStar2);
+$stars[3].addEventListener("click", handleClickStar3);
+$stars[4].addEventListener("click", handleClickStar4);
+
+
+function handleClickHeart() {
     $heart.classList.toggle('-active');
 };
 
 
-function handleClickFirstStar() {
-    $firstStar.classList.toggle("-active");
+function handleClickStar0() {
+    $stars[0].classList.toggle("-active");
+};
+
+function handleClickStar1() {
+    $stars[1].classList.toggle("-active");
+};
+
+function handleClickStar2() {
+    $stars[2].classList.toggle("-active");
+};
+
+function handleClickStar3() {
+    $stars[3].classList.toggle("-active");
+};
+
+function handleClickStar4() {
+    $stars[4].classList.toggle("-active");
 };
